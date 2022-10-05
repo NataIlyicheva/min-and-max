2836a00704e529a5e17e28d625615cfd3e8944b2
Natali <n7483205@ya.ru>
2022-10-05 21:30:52 +0700

    min and max

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите первое число.");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число.");
+int number2 = int.Parse(Console.ReadLine()!);
+
+if (number1 > number2)
+{
+    Console.WriteLine($"{number1} максимальное число");
+    Console.WriteLine($"{number2} минимальное число");
+}
+else{
+    Console.WriteLine($"{number2} максимальное число");
+    Console.WriteLine($"{number1} минимальное число");
+}
